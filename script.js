1f4cf074bfeded88dcaa49d2ccdd847741238903
vitorbretz <vitorbretz28@gmail.com>
2025-03-06 22:53:50 -0300

    js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const element =  document.querySelectorAll('.conteudo img')
-element.forEach((item)=>{
-    item.addEventListener('click', randomNumber)
+const element =  document.querySelectorAll('.conteudo img');
+const spanText =  document.querySelector('#about');
+const resultT = document.querySelector('.resultado');
+
+//moves
+const playPedra = document.querySelector('.playPedra');
+const playPapel = document.querySelector('.playPapel');
+const playTesoura = document.querySelector('.PlayTesoura');
+
+playPedra.addEventListener('click', ()=>{
+    playGame('Pedra')
+})
+playPapel.addEventListener('click', ()=>{
+    playGame('Papel')
+})
+playTesoura.addEventListener('click', ()=>{
+    playGame('Tesoura')
 })
 
 function randomNumber(){
@@ -13,6 +27,40 @@ function randomNumber(){
     }else{
         computerMove = 'Tesoura'
     }
-    console.log(computerMove)
-        
+
+    return computerMove;
+    
+}
+function playGame(playerMove){
+    const computerMove = randomNumber();
+
+    let result = '';
+    if (playerMove === 'Tesoura' ){
+        if (computerMove === 'Pedra'){
+            result = 'Você perdeu'
+        }else if (computerMove === 'Papel'){
+            result = 'Você ganhou'
+        }else if (computerMove === 'Tesoura'){
+            result = 'Empate'
+        }
+    }else if (playerMove === 'Pedra' ){
+        if (computerMove === 'Pedra'){
+            result = 'Empate'
+        }else if (computerMove === 'Papel'){
+            result = 'Você perdeu'
+        }else if (computerMove === 'Tesoura'){
+            result = 'Você ganhou'
+        }
+    }else if (playerMove === 'Papel' ){
+        if (computerMove === 'Pedra'){
+            result = 'Você ganhou'
+        }else if (computerMove === 'Papel'){
+            result = 'Empate'
+        }else if (computerMove === 'Tesoura'){
+            result = 'Voce perdeu'
+        }
+    }
+    
+    spanText.innerText = (`Você escolheu ${playerMove} é a maquina escolheu ${computerMove}`)
+    resultT.innerText = result
 }
